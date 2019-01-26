bad044da198ad09460c1c1019aeee68a6fb52974
niceherui <heruioo@gmail.com>
2019-01-26 17:27:13 +0800

    fix

--- js/productInfo.js
+++ js/productInfo.js
@@ -5,6 +5,9 @@ $(function () {
     main_img_url: "http://www.pokemon.com/images/product-vg@1.png"
     name: "芹菜 半斤"
     price: "0.01"*/
+
+    getProductInfo();
+
     $('#add,#min').click(function () {
         var count = $('#text_box').val();
         var totalPrice = productInfoData.price * count;
@@ -12,7 +15,66 @@ $(function () {
     });
 
 
-    getProductInfo();
+
+    // var privArr = [];
+    // privArr['staProjQueryGrid'] = [{
+    //     btn_id : 'but_add',
+    //     roles : ['2001','2005']
+    // }]
+
+    $('#shopCar').click(function () {
+        //window.base.deleteLocalStorage('shopCar');
+        var shopcar = localStorage.getItem('shopCar');
+        var shopCache = JSON.parse(shopcar);
+
+        var price = Number($('#price1').text());
+        var count = Number($('#text_box').val());
+
+
+        var shopCarArr = [];
+
+        //判断缓存中是否有购物车信息
+        if (shopCache) {
+            var cachePrice = Number(shopCache.price);
+            var cacheCount = Number(shopCache.count);
+
+            if (shopCache.id == productInfoData.id) {
+
+                shopCarArr[productInfoData.id] = [{
+                    id: productInfoData.id,
+                    img_id: productInfoData.img_id,
+                    main_img_url: productInfoData.main_img_url,
+                    price: (parseInt(cachePrice * 100) + parseInt(price * 100))/100,
+                    count: cacheCount + count,
+                }];
+                
+                console.log(price);
+                console.log(cachePrice);
+            } else {
+
+            }
+        } else {
+            //增加新的缓存
+            shopCarArr[productInfoData.id] = [{
+                id: productInfoData.id,
+                img_id: productInfoData.img_id,
+                main_img_url: productInfoData.main_img_url,
+                price: price,
+                count: count,
+            }];
+
+        }
+/*
+        localStorage.setItem('shopCar', JSON.stringify(data));
+        var shopcar= localStorage.getItem('shopCar');*/
+
+        console.log(shopCarArr);
+    });
+
+
+
+
+
 
     function getProductInfo() {
         var str = GetQueryString('id');
@@ -45,9 +107,6 @@ $(function () {
 
     /**
      * 获取地址栏中的指定参数
-     * @param name
-     * @returns {*}
-     * @constructor
      */
     function GetQueryString(name)
     {
